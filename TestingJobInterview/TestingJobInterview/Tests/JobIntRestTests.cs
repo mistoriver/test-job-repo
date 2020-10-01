373ec2bf40b878709226920074a4b4e0b73dc589
mistoriver <guffmc@gmail.com>
2020-10-01 13:28:50 +0300

    Поисковая строка вынесена из тестов

--- TestingJobInterview/TestingJobInterview/Tests/JobIntRestTests.cs
+++ TestingJobInterview/TestingJobInterview/Tests/JobIntRestTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System.Reflection.Metadata;
 using TestingJobInterview.APIEntities;
 
 namespace TestingJobInterview
@@ -6,19 +7,23 @@ namespace TestingJobInterview
     [TestFixture]
     public class JobIntRestTests
     {
+        const string anySearch = "LiNq";
+        readonly string upperSearch = anySearch.ToUpper();
+        readonly string lowerSearch = anySearch.ToLower();
+
         #region Самый толковый тест
         [Test]
         public void AnyLinqTest()
         {
             int resultCountNeedToTest = 500;
 
-            var results = Searcher.Search("LINQ", resultCountNeedToTest);
+            var results = Searcher.Search(anySearch, resultCountNeedToTest);
 
             Assert.IsTrue(results.Count > 0);
 
             results.ForEach(res => Assert.IsTrue(
-                res.Title.ToLower().Contains("linq") ||
-                res.Description.ToLower().Contains("linq")
+                res.Title.ToLower().Contains(lowerSearch) ||
+                res.Description.ToLower().Contains(lowerSearch)
                 )
             );
         }
@@ -28,7 +33,7 @@ namespace TestingJobInterview
         public void UpperLinqTest()
         {
             //API сайта отбивает запросы с top>25, поэтому пара стандартных проверок
-            RestSearchHelper.ConfigureDefaultSearch("LINQ", 25);
+            RestSearchHelper.ConfigureDefaultSearch(upperSearch, 25);
 
             var response = RestSearchHelper.Execute();
 
@@ -40,15 +45,15 @@ namespace TestingJobInterview
             Assert.IsTrue(results.Count > 0);
 
             results.ForEach(res => Assert.IsTrue(
-                res.Title.ToLower().Contains("linq") ||
-                res.Description.ToLower().Contains("linq")
+                res.Title.ToLower().Contains(lowerSearch) ||
+                res.Description.ToLower().Contains(lowerSearch)
                 )
             );
         }
         [Test]
         public void LowerLinqTest()
         {
-            RestSearchHelper.ConfigureDefaultSearch("linq", 25);
+            RestSearchHelper.ConfigureDefaultSearch(lowerSearch, 25);
 
             var response = RestSearchHelper.Execute();
 
@@ -60,15 +65,15 @@ namespace TestingJobInterview
             Assert.IsTrue(results.Count > 0);
 
             results.ForEach(res => Assert.IsTrue(
-                res.Title.ToLower().Contains("linq") ||
-                res.Description.ToLower().Contains("linq")
+                res.Title.ToLower().Contains(lowerSearch) ||
+                res.Description.ToLower().Contains(lowerSearch)
                 )
             );
         }
         [Test]
         public void JumpingLinqTest()
         {
-            RestSearchHelper.ConfigureDefaultSearch("lInQ", 25);
+            RestSearchHelper.ConfigureDefaultSearch(anySearch, 25);
 
             var response = RestSearchHelper.Execute();
 
@@ -80,8 +85,8 @@ namespace TestingJobInterview
             Assert.IsTrue(results.Count > 0);
 
             results.ForEach(res => Assert.IsTrue(
-                res.Title.ToLower().Contains("linq") ||
-                res.Description.ToLower().Contains("linq")
+                res.Title.ToLower().Contains(lowerSearch) ||
+                res.Description.ToLower().Contains(lowerSearch)
                 )
             );
         }
@@ -89,7 +94,7 @@ namespace TestingJobInterview
         public void FiftyLinqTest()
         {
             //А здесь я придумал самый простой способ, как сформировать 50 записей
-            RestSearchHelper.ConfigureDefaultSearch("LINQ", 25);
+            RestSearchHelper.ConfigureDefaultSearch(upperSearch, 25);
 
             var response = RestSearchHelper.Execute();
             
@@ -97,7 +102,7 @@ namespace TestingJobInterview
             Assert.AreEqual(System.Net.HttpStatusCode.OK, response.StatusCode);
 
 
-            RestSearchHelper.ConfigureSearch("LINQ", 25, 25);
+            RestSearchHelper.ConfigureSearch(upperSearch, 25, 25);
             var secResponse = RestSearchHelper.Execute();
 
             Assert.IsNotNull(secResponse);
@@ -108,8 +113,8 @@ namespace TestingJobInterview
             Assert.IsTrue(results.Count > 0);
 
             results.ForEach(res => Assert.IsTrue(
-                res.Title.ToLower().Contains("linq") ||
-                res.Description.ToLower().Contains("linq")
+                res.Title.ToLower().Contains(lowerSearch) ||
+                res.Description.ToLower().Contains(lowerSearch)
                 )
             );
         }
